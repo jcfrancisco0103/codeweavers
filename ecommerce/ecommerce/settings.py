ff558b9304ebdf991b2df0f9588250dbbf0af8d5
Jan Christopher Francisco <152581275+jcfrancisco0103@users.noreply.github.com>
2025-03-09 16:39:59 +0000

    deployment

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = '#vw(03o=(9kbvg!&2d5i!2$_58x@_-3l4wujpow6(ym37jxnza'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ["codeweavers.vercel.app", "codeweavers-d6rj61bno-jcfrancisco0103s-projects.vercel.app"]
 
 
 # Application definition
